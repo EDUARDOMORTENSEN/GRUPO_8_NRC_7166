b08079f20ca19389cc234d368ad3730777474914
Eduardo Antonio Mortensen Franco <84451615+EDUARDOMORTENSEN@users.noreply.github.com>
2021-10-29 17:52:45 -0500

    TRABAJO EXTRA 1

---
+++ Proceso.cpp
@@ -0,0 +1,95 @@
+/***********************************************************************
+ * Module:  Proceso.cpp
+ * Author:  pc
+ * Modified: jueves, 28 de octubre de 2021 19:19:43
+ * Purpose: Implementation of the class Proceso
+ ***********************************************************************/
+
+#include "Operaciones.cpp.h"
+#include "Proceso.h"
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Proceso::getDenominador()
+// Purpose:    Implementation of Proceso::getDenominador()
+// Return:     float
+////////////////////////////////////////////////////////////////////////
+
+float Proceso::getDenominador(void)
+{
+   return denominador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Proceso::setDenominador(float newDenominador)
+// Purpose:    Implementation of Proceso::setDenominador()
+// Parameters:
+// - newDenominador
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Proceso::setDenominador(float newDenominador)
+{
+   denominador = newDenominador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Proceso::getNumerador()
+// Purpose:    Implementation of Proceso::getNumerador()
+// Return:     float
+////////////////////////////////////////////////////////////////////////
+
+float Proceso::getNumerador(void)
+{
+   return numerador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Proceso::setNumerador(float newNumerador)
+// Purpose:    Implementation of Proceso::setNumerador()
+// Parameters:
+// - newNumerador
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Proceso::setNumerador(float newNumerador)
+{
+   numerador = newNumerador;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Proceso::Proceso(float num, float dem)
+// Purpose:    Implementation of Proceso::Proceso()
+// Parameters:
+// - num
+// - dem
+// Return:
+////////////////////////////////////////////////////////////////////////
+
+Proceso::Proceso(float num, float dem)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Proceso::~Proceso()
+// Purpose:    Implementation of Proceso::~Proceso()
+// Return:
+////////////////////////////////////////////////////////////////////////
+
+Proceso::~Proceso()
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Proceso::imprimir(Proceso objeto)
+// Purpose:    Implementation of Proceso::imprimir()
+// Parameters:
+// - objeto
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Proceso::imprimir(Proceso objeto)
+{
+   // TODO : implement
+}
