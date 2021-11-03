71cc29a75d9d55f26483615fd12db8948adf584f
Eduardo Antonio Mortensen Franco <84451615+EDUARDOMORTENSEN@users.noreply.github.com>
2021-11-03 17:58:24 -0500

    trabajo extra ejercicio 6

---
+++ Eduardo Mortensen Trabajo Extra/trabajo_extra_1_ejercicio_6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+ void Velocidad(T distancia, T tiempo);
+
+
+template < typename T>
+ void Velocidad(T distancia, T tiempo){
+ T resultado;
+ resultado= distancia/tiempo;
+ cout<<"el resultado del calculo de la velocidad es:"<<resultado<<endl;
+ }
+
+int main ()
+{
+    float w = 12;
+    float s = 60;
+
+    double q1 = 28.7;
+    double q2 = 6.6;
+
+    Velocidad(w,s);
+    Velocidad(q1,q2);
+
+   return 0;
+}
