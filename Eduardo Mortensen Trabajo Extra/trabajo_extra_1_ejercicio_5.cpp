d2a9aca1514e90b5a0fd29a10e05525b3efff873
Eduardo Antonio Mortensen Franco <84451615+EDUARDOMORTENSEN@users.noreply.github.com>
2021-11-03 17:48:15 -0500

    trabajo extra ejercicio 5

---
+++ Eduardo Mortensen Trabajo Extra/trabajo_extra_1_ejercicio_5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+ void AreaDelTriangulo(T base, T altura);
+
+
+template < typename T>
+ void AreaDelTriangulo(T base, T altura){
+ T resultado;
+ resultado= (base+altura)/2;
+ cout<<"el resultado del area del triangulo es "<<resultado<<endl;
+ }
+
+int main ()
+{
+    int x = 4;
+    int y = 3;
+
+    double z1 = 3.7;
+    double z2 = 4.6;
+
+    AreaDelTriangulo(x,y);
+    AreaDelTriangulo(z1,z2);
+
+   return 0;
+}
