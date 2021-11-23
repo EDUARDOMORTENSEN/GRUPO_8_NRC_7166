7424fcd52c5640635135ccbd922df738494e554f
EscobarIsaac <iaescobar3@espe.edu.ec>
2021-11-23 18:24:39 -0500

    lista simple

---
+++ 1Parcial/Lista Simple/main.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "Lista.h"
+using namespace std;
+int main(int argc, char** argv) {
+	Lista lst;
+	lst.insertar(3);
+	lst.insertar(13);
+	lst.insertar(23);
+	lst.insertar(33);
+	lst.insertar(43);
+	lst.mostrar();
+	return 0;
+}
